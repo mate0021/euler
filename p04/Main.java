b63481cbf341a181978339d3bf1eb78e65306ae8
mate0021 <mami.ext@gmail.com>
2019-11-23 17:26:01 +0100

    Daily Euler. Dirty solution for palindromic product.

--- p04/Main.java
+++ p04/Main.java
@@ -2,24 +2,44 @@ public class Main {
 
     public static void main(String[] args) {
 
+        List<Integer> palindromes = new ArrayList<>();
+
+        int result = 0;
         outer:
-        for (int i = 99; i > 1; i--) {
-            for (int j = 99; j > 1; j--) {
-                System.out.println(i * j);
-                if ((i * j) / 1000 < 1) {
-                    break outer;
+        for (int i = 999; i > 1; i--) {
+            for (int j = 999; j > 1; j--) {
+                if ((i * j) / 100000 < 1) {
+                    break;
+                } else
+                if (isPalindromic(i * j)) {
+                    result = i * j;
+                    System.out.println(String.format(" >> %d * %d = %d", i, j, i * j));
+                    palindromes.add(result);
+//                    break outer; // <-- DONT BREAK. We don't know if this is the largest palindrome! (995 * 583) < (993 * 913)
                 }
             }
         }
 
-        int input = 5234;
-        System.out.println( (input / 100) );
-        int pos = 2;
-        for (int x = 3; x > 0; x--) {
-            if (x == pos) {
+        System.out.println(palindromes.size());
+        System.out.println(palindromes.stream().max(Comparator.naturalOrder()));
+    }
+    
+    private static int getDigitAtPosition(int input, int position) {
+        int result = input / (int) pow(10, position);
+        result %= 10;
 
-            }
-        }
+        return result;
+    }
+
+    private static boolean isPalindromic(int input) {
+        int numOfDigits = getNumberOfDigits(input);
+        return getDigitAtPosition(input, 0) == getDigitAtPosition(input, 5) &&
+                getDigitAtPosition(input, 1) == getDigitAtPosition(input, 4) &&
+                getDigitAtPosition(input, 2) == getDigitAtPosition(input, 3);
+    }
+
+    private static int getNumberOfDigits(int input) {
+        return (int) Math.log10(input) + 1;
     }
 }
 /*
