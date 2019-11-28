1cc8d289ff6b980e961a0055974a51daf21127a1
mate00 <mami.ext@gmail.com>
2019-11-28 06:59:49 +0100

    Daily Code Wars. Brace checker.

---
+++ valid_braces/src/rnd/mate00/codewars/BraceChecker.java
@@ -0,0 +1,43 @@
+package rnd.mate00.codewars;
+
+import java.util.Stack;
+
+/**
+ * https://www.codewars.com/kata/valid-braces/train/java
+ */
+public class BraceChecker {
+
+    public boolean isValid(String braces) {
+        Stack<Character> stack = new Stack<>();
+        if (braces.isEmpty()) {
+            return true;
+        } else {
+            char[] chars = braces.toCharArray();
+            stack.push(chars[0]);
+            for (int x = 1; x < chars.length; x++) {
+                if (isOpeningBracket(chars[x])) {
+                    stack.push(chars[x]);
+                } else if (isClosingBracket(chars[x]) && !isMatching(stack.pop(), chars[x])) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    private boolean isOpeningBracket(char bracket) {
+        return bracket == '(' || bracket == '[' || bracket == '{';
+    }
+
+    private boolean isClosingBracket(char bracket) {
+        return bracket == ')' || bracket == ']' || bracket == '}';
+    }
+
+    private boolean isMatching(char left, char right) {
+        return left == '(' && right == ')' ||
+                left == '[' && right == ']' ||
+                left == '{' && right == '}';
+    }
+
+}
---
+++ valid_braces/src/rnd/mate00/codewars/BraceCheckerTest.java
@@ -0,0 +1,44 @@
+package rnd.mate00.codewars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BraceCheckerTest {
+
+    private BraceChecker checker = new BraceChecker();
+
+    @Test
+    public void valid() {
+        assertEquals(true, checker.isValid("()()"));
+    }
+
+    @Test
+    public void validSequence() {
+        assertTrue(checker.isValid("(){}[]"));
+    }
+
+    @Test
+    public void validNested() {
+        assertTrue(checker.isValid("([{}])"));
+        assertTrue(checker.isValid("[({})]"));
+    }
+
+    @Test
+    public void validNestedSequence() {
+        assertTrue(checker.isValid("([]){{}}"));
+    }
+
+    @Test
+    public void validNestedPlusSequence() {
+        assertTrue(checker.isValid("(){}[()]"));
+    }
+
+    @Test
+    public void testInvalid() {
+        assertEquals(false, checker.isValid("[(])"));
+        assertFalse(checker.isValid("[][{()]"));
+        assertFalse(checker.isValid("[{(})]"));
+        assertFalse(checker.isValid("()()["));
+    }
+}
