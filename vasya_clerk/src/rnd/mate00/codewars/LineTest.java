705850f3c4ec26aeb7115e7b217f36d8944e4004
mate00 <mami.ext@gmail.com>
2019-12-03 07:16:46 +0100

    Daily Code Wars. Vasya clerk. Tests.

--- vasya_clerk/src/rnd/mate00/codewars/LineTest.java
+++ vasya_clerk/src/rnd/mate00/codewars/LineTest.java
@@ -16,4 +16,40 @@ public class LineTest {
         assertEquals("NO", Line.Tickets(new int []{25, 100}));
     }
 
+    @Test
+    public void notPossibleToHandleAllQueue1() {
+        assertEquals("NO", Line.Tickets(new int[] {100, 50, 25, 25}));
+    }
+
+    @Test
+    public void notPossibleToHandleAllQueue3() {
+        assertEquals("NO", Line.Tickets(new int[] {25, 25, 50, 50, 100}));
+    }
+
+    @Test
+    public void notPossibleToHandleAllQueue4() {
+        assertEquals("NO", Line.Tickets(new int[] {50, 50, 50, 50, 50, 50, 50, 50, 50, 50}));
+    }
+
+    @Test
+    public void notPossibleToHandleAllQueue5() {
+        assertEquals("NO", Line.Tickets(new int[] {100, 100, 100, 100, 100, 100, 100, 100, 100, 100}));
+    }
+
+    @Test
+    public void notPossibleToHandleAllQueue6() {
+        assertEquals("NO", Line.Tickets(new int[] {25, 25, 25, 25, 25, 25, 25, 50, 50, 50, 100, 100, 100, 100}));
+    }
+
+    @Test
+    public void notPossibleToHandleAllQueue2() {
+        assertEquals("NO", Line.Tickets(new int[] {25, 25, 25, 25, 25, 100, 100}));
+    }
+
+    @Test
+    public void shouldHandleAllQueue() {
+        assertEquals("YES", Line.Tickets(new int[] {25, 25, 25, 100}));
+
+    }
+
 }
