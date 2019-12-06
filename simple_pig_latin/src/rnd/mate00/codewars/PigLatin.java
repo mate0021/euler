8931eb4d4496a8e492b9421a59cf7aea8ca85061
mate00 <mami.ext@gmail.com>
2019-12-06 07:02:02 +0100

    Daily Code Wars. Pig Latin.

--- simple_pig_latin/src/rnd/mate00/codewars/PigLatin.java
+++ simple_pig_latin/src/rnd/mate00/codewars/PigLatin.java
@@ -1,5 +1,10 @@
 package rnd.mate00.codewars;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
 /**
  * https://www.codewars.com/kata/simple-pig-latin/train/java
  *
@@ -19,6 +24,26 @@ public class PigLatin {
     }
 
     private static String swapLetter(String word) {
+        List<Character> chars = new ArrayList<>();
+        for (char c : word.toCharArray()) {
+            chars.add(c);
+        }
+        String newSuffix = chars.get(0) + "ay";
+
+        for (int i = 1; i < word.length(); i++) {
+            chars.add(i - 1, word.charAt(i));
+        }
+
+        char swap;
+        for (int i = 0; i < word.length(); i++) {
+            if (isCharacter(word.charAt(i))) {
+
+            }
+        }
         return word;
+    }
+
+    private static boolean isCharacter(Character c) {
+        return Pattern.matches("\\w", String.valueOf(c));
     }
 }
