dac8e0287f4f7f71e45b3ad60949743628698de2
mate0021 <mami.ext@gmail.com>
2019-12-09 15:42:55 +0100

    Daily Codewars. The observed PIN.

---
+++ the_observed_pin/src/rnd/mate00/codewars/ObservedPin.java
@@ -0,0 +1,43 @@
+package rnd.mate00.codewars;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Arrays.asList;
+
+public class ObservedPin {
+
+    private static final Map<Character, List<String>> neighbours = new HashMap<>() {{
+        put('1', asList("1", "2", "4"));
+        put('3', asList("3", "2", "6"));
+
+
+
+
+        put('2', asList("2", "1", "3", "5"));
+
+        put('4', asList("4", "1", "5", "7"));
+        put('5', asList("5", "2", "4", "6", "8"));
+        put('6', asList("6", "3", "5", "9"));
+        put('7', asList("7", "4", "8"));
+        put('8', asList("8", "5", "7", "9", "0"));
+        put('9', asList("9", "6", "8"));
+        put('0', asList("0", "8"));
+    }};
+
+    public static List<String> getPINs(String observed) {
+        System.out.println(observed);
+        List<String> result = new ArrayList<>();
+        for (char c : observed.toCharArray()) {
+//            neighbours.get(c).forEach();
+        }
+
+        return result;
+    }
+
+    /*
+    13 -> 13, 12, 16, 23, 22, 26, 43, 42, 46
+     */
+}
