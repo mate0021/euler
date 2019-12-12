d0f7240e3856be1151d5550dfdd9d2325829262c
mate0021 <mami.ext@gmail.com>
2019-12-12 14:26:55 +0100

    Daily Codewars. Human readable duration format.

---
+++ human_readable_duration_format/src/rnd/mate00/codewars/TimeFormatter.java
@@ -0,0 +1,13 @@
+package rnd.mate00.codewars;
+
+/**
+ * https://www.codewars.com/kata/human-readable-duration-format/train/java
+ *
+ */
+public class TimeFormatter {
+
+    public static String formatDuration(int seconds) {
+        return "now";
+    }
+
+}
