c197c9c844b3c544ff133ad4f01dbbf4236c7b02
mate00 <mami.ext@gmail.com>
2019-12-16 07:19:31 +0100

    Daily Code Wars. BrainLuck.

---
+++ my_smallest_code_interpreter/src/rnd/mate00/codewars/BrainLuck.java
@@ -0,0 +1,15 @@
+package rnd.mate00.codewars;
+
+/**
+ * https://www.codewars.com/kata/my-smallest-code-interpreter-aka-brainf-star-star-k/train/java
+ *
+ */
+public class BrainLuck {
+
+    public BrainLuck(String code) {
+    }
+
+    public String process(String input) {
+        return "";
+    }
+}
