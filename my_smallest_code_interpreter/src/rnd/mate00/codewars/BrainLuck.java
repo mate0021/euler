3d126bfdc86117403f3a02c1256ca51effa4bc61
mate0021 <mami.ext@gmail.com>
2019-12-17 14:03:48 +0100

    Daily Codewars. Brain Luck initial loop.

--- my_smallest_code_interpreter/src/rnd/mate00/codewars/BrainLuck.java
+++ my_smallest_code_interpreter/src/rnd/mate00/codewars/BrainLuck.java
@@ -6,10 +6,63 @@ package rnd.mate00.codewars;
  */
 public class BrainLuck {
 
+    private byte[] mem;
+
+    private char[] code;
+
     public BrainLuck(String code) {
+        mem = new byte[1024];
+        initMemoryBlock();
+        this.code = code.toCharArray();
+        System.out.println("Loaded code: " + code);
     }
 
     public String process(String input) {
-        return "";
+        System.out.println("Processing input " + input);
+        StringBuilder console = new StringBuilder();
+
+        int ptr = 0;
+        for (int c = 0; c < code.length; c++) {
+            char instruction = code[c];
+            switch (instruction) {
+                case '>':
+                    ++ptr;
+                    break;
+                case '<':
+                    --ptr;
+                    break;
+                case '+':
+                    if (mem[ptr] == 255) {
+                        mem[ptr] = 0;
+                    } else {
+                        mem[ptr]++;
+                    }
+                    break;
+                case '-':
+                    if (mem[ptr] == 0) {
+                        mem[ptr] = (byte) 255;
+                    } else {
+                        mem[ptr]--;
+                    }
+                    break;
+                case '.':
+                    console.append(mem[ptr]);
+                    break;
+                case ',':
+                    break;
+                case '[':
+                    break;
+                case ']':
+                    break;
+            }
+        }
+
+        return console.toString();
+    }
+
+    private void initMemoryBlock() {
+        for (int i = 0; i < mem.length; i++) {
+            mem[i] = 0;
+        }
     }
 }
