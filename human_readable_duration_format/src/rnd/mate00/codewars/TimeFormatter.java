c64dd0169158243e8b44e165851000d9d022a713
mate0021 <mami.ext@gmail.com>
2019-12-18 12:10:15 +0100

    Daily Codewars. Human readable time formatter.

--- human_readable_duration_format/src/rnd/mate00/codewars/TimeFormatter.java
+++ human_readable_duration_format/src/rnd/mate00/codewars/TimeFormatter.java
@@ -1,6 +1,8 @@
 package rnd.mate00.codewars;
 
-import java.time.temporal.ChronoUnit;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -12,7 +14,6 @@ public class TimeFormatter {
     private static final int SECS_IN_MINUTES = 60;
     private static final int SECS_IN_HOURS = 60 * SECS_IN_MINUTES;
     private static final int SECS_IN_DAYS = 24 * SECS_IN_HOURS;
-    private static final int SECS_IN_YEAR = 365 * SECS_IN_DAYS;
     private static final int DAYS_IN_YEAR = 365;
 
     public static String formatDuration(int secondsTotal) {
@@ -22,29 +23,47 @@ public class TimeFormatter {
             return "now";
         }
 
-        long years = 0;
-        long days = 0;
-        long hours = 0;
-        long minutes = 0;
-        long seconds = 0;
+        long years, days, hours, minutes, seconds;
 
-        int test = SECS_IN_DAYS * 365 * 2 + 54 * SECS_IN_DAYS + 13 * SECS_IN_HOURS + 74 * SECS_IN_MINUTES + 14;
-
-        days = TimeUnit.SECONDS.toDays(test);
-        test -= days * SECS_IN_DAYS;
+        days = TimeUnit.SECONDS.toDays(secondsTotal);
+        secondsTotal -= days * SECS_IN_DAYS;
 
         years = days / DAYS_IN_YEAR;
         days -= years * DAYS_IN_YEAR;
 
-        hours = TimeUnit.SECONDS.toHours(test);
-        test -= hours * SECS_IN_HOURS;
+        hours = TimeUnit.SECONDS.toHours(secondsTotal);
+        secondsTotal -= hours * SECS_IN_HOURS;
 
-        minutes = TimeUnit.SECONDS.toMinutes(test);
-        seconds = test - minutes * SECS_IN_MINUTES;
+        minutes = TimeUnit.SECONDS.toMinutes(secondsTotal);
+        seconds = secondsTotal - minutes * SECS_IN_MINUTES;
 
+        return formatTime(years, days, hours, minutes, seconds);
+    }
 
-        System.out.println(String.format("%d years, %d days, %d hours, %d minutes, %d seconds", years, days, hours, minutes, seconds));
-        return "now";
+    private static String formatTime(long years, long days, long hours, long minutes, long seconds) {
+        List<String> parts = new ArrayList<>();
+        if (years > 0) {
+            parts.add(years + (years == 1 ? " year" : " years"));
+        }
+        if (days > 0) {
+            parts.add(days + (days == 1 ? " day" : " days"));
+        }
+        if (hours > 0) {
+            parts.add(hours + (hours == 1 ? " hour" : " hours"));
+        }
+        if (minutes > 0) {
+            parts.add(minutes + (minutes == 1 ? " minute" : " minutes"));
+        }
+        if (seconds > 0) {
+            parts.add(seconds + (seconds == 1 ? " second" : " seconds"));
+        }
+
+        if (parts.size() == 1) {
+            return parts.get(0);
+        }
+
+        int last = parts.size() - 1;
+        return String.join(" and ", String.join(", ", parts.subList(0, last)), parts.get(last));
     }
 
 }
