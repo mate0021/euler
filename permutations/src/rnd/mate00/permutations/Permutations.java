1ef72e6bc5bdd9729aa2c9c928601b2f7060b85d
mate00 <mami.ext@gmail.com>
2019-12-24 09:07:16 +0100

    Daily Code Wars. Permutations.

--- permutations/src/rnd/mate00/permutations/Permutations.java
+++ permutations/src/rnd/mate00/permutations/Permutations.java
@@ -1,15 +1,45 @@
 package rnd.mate00.permutations;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * https://www.codewars.com/kata/permutations/train/java
+ * https://introcs.cs.princeton.edu/java/23recursion/
  *
  */
 public class Permutations {
 
     public static List<String> singlePermutations(String input) {
+        char[] chars = input.toCharArray();
+        Set<String> result = new HashSet<>();
+
+        //result = ...
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            // result.add(permute(c, list_of_others_than_c));
+        }
+        System.out.println(permute("", input));
         return Arrays.asList("ab", "ba");
     }
+    /*
+    abc
+    perm(abc) = [a, perm(bc)]
+    perm(bc) = [b, perm(c)]
+    perm(c) = [c]
+     */
+    static String permute(String prefix, String in) {
+        if (in.length() == 0) {
+            return prefix;
+        }
+        String result = "";
+        for (int i = 0; i < in.length(); i++) {
+            result = permute(prefix + in.charAt(i), in.substring(0, i) + in.substring(i + 1, in.length()));
+        }
+
+        return result;
+    }
+
 }
