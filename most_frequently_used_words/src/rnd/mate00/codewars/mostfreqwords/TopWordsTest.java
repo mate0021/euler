74f917ae12d38f927cef7706e8485aa2dea9ea94
mate00 <mami.ext@gmail.com>
2019-12-26 08:58:13 +0100

    Daily Code Wars. Most frequently used words.

--- most_frequently_used_words/src/rnd/mate00/codewars/mostfreqwords/TopWordsTest.java
+++ most_frequently_used_words/src/rnd/mate00/codewars/mostfreqwords/TopWordsTest.java
@@ -13,13 +13,6 @@ public class TopWordsTest {
 
     @Test
     public void sampleTests() {
-        assertEquals(Arrays.asList("e", "d", "a"),    TopWords.top3("a a a  b  c c  d d d d  e e e e e"));
-        assertEquals(Arrays.asList("e", "ddd", "aa"), TopWords.top3("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"));
-        assertEquals(Arrays.asList("won't", "wont"),  TopWords.top3("  //wont won't won't "));
-        assertEquals(Arrays.asList("e"),              TopWords.top3("  , e   .. "));
-        assertEquals(Arrays.asList(),                 TopWords.top3("  ...  "));
-        assertEquals(Arrays.asList(),                 TopWords.top3("  '  "));
-        assertEquals(Arrays.asList(),                 TopWords.top3("  '''  "));
         assertEquals(Arrays.asList("a", "of", "on"),  TopWords.top3(Stream
                 .of("In a village of La Mancha, the name of which I have no desire to call to",
                         "mind, there lived not long since one of those gentlemen that keep a lance",
@@ -28,5 +21,32 @@ public class TopWordsTest {
                         "nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra",
                         "on Sundays, made away with three-quarters of his income.")
                 .collect(Collectors.joining("\n")) ));
+    }
+
+    @Test
+    public void t1() {
+        assertEquals(Arrays.asList("e", "ddd", "aa"), TopWords.top3("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"));
+    }
+
+    @Test
+    public void t2() {
+        assertEquals(Arrays.asList("e", "d", "a"), TopWords.top3("a a a  b  c c  d d d d  e e e e e"));
+    }
+
+    @Test
+    public void t3() {
+        assertEquals(Arrays.asList("won't", "wont"), TopWords.top3("  //wont won't won't "));
+    }
+
+    @Test
+    public void t4() {
+        assertEquals(Arrays.asList("e"), TopWords.top3("  , e   .. "));
+    }
+
+    @Test
+    public void t5() {
+        assertEquals(Arrays.asList(), TopWords.top3("  ...  "));
+        assertEquals(Arrays.asList(), TopWords.top3("  '  "));
+        assertEquals(Arrays.asList(), TopWords.top3("  '''  "));
     }
 }
