edb04a41ed13178c0226c8c5f52b8fffeb1da585
mate00 <mami.ext@gmail.com>
2019-12-26 08:54:34 +0100

    Daily Code Wars. Most frequently used words.

---
+++ most_frequently_used_words/src/rnd/mate00/codewars/mostfreqwords/TopWords.java
@@ -0,0 +1,15 @@
+package rnd.mate00.codewars.mostfreqwords;
+
+import java.util.*;
+
+/**
+ * https://www.codewars.com/kata/most-frequently-used-words-in-a-text/train/java
+ *
+ */
+public class TopWords {
+
+    public static List<String> top3(String s) {
+        // Your code here
+        return null;
+    }
+}
