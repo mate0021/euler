aa2e076876d951bc3986edbbf69935dedc1ac451
mate0021 <mami.ext@gmail.com>
2020-11-03 07:46:59 +0100

    Daily Groovy. Better solution.

--- ordered_count_of_characters/src/rnd/mate00/codewars/Kata.groovy
+++ ordered_count_of_characters/src/rnd/mate00/codewars/Kata.groovy
@@ -1,6 +1,5 @@
 package rnd.mate00.codewars
 
-import java.util.function.IntFunction;
 
 /*
  * https://www.codewars.com/kata/57a6633153ba33189e000074/train/groovy
@@ -9,6 +8,29 @@ import java.util.function.IntFunction;
 class Kata {
 
     static def orderedCount(input) {
+        input.toList()
+                .groupBy {it} // ['a' : ['a', 'a', 'a'], 'b' : ['b', 'b']]
+                .collect {
+                    [it.getKey(), it.getValue().size()]
+                }
+    }
+
+    static def orderedCount2(input) {
+        def tmpMap = [:]
+        input.each {
+            if (!tmpMap.containsKey(it)) {
+                tmpMap[it] = 1
+            } else {
+                tmpMap[it] += 1
+            }
+        }
+
+        tmpMap.collect {
+            [it.getKey(), it.getValue()]
+        }
+    }
+
+    static def orderedCountMy(input) {
         if (input.isEmpty()) {
             []
         } else {
